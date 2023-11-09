0b6619c6717721bb5828e24d84f3e791560f5e1e
JuanJSMVM <juan020405@hotmail.com>
2023-11-09 10:18:54 -0500

    Primer commit

---
+++ src/main/java/com/example/ajedrezonline/AjedrezOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.ajedrezonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjedrezOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjedrezOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ajedrezonline/AjedrezOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ajedrezonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjedrezOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
